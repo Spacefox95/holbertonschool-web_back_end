abd66140844b28617e9239a42d6c2de1a76e826e
Spacefox95 <nathan.raynal@gmail.com>
2024-04-22 14:36:11 +0200

    Adding a loop is better

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,5 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int):
     """ Function creating a task"""
-    task: asyncio.task = asyncio.create_task(wait_random(max_delay))
+    loop = asyncio.get_event_loop()
+    task: asyncio.task = loop.create_task(wait_random(max_delay))
     return task
