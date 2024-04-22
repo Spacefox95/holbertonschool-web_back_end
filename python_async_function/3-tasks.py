05caa1214518ef7f707c93515e4327a94087ac37
Spacefox95 <nathan.raynal@gmail.com>
2024-04-22 14:24:23 +0200

    New_task

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Create a task as wait_random is working
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """ Function creating a task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
