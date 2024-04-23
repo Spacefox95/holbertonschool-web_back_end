8c1f4ecd2ad03c23eca70beec6c14510c456967f
Spacefox95 <nathan.raynal@gmail.com>
2024-04-23 12:01:06 +0200

    Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ Yield a random number """
     for i in range(10):
         await asyncio.sleep(1)
