a099861bd43463589f8eab69b439989737c4511f
Spacefox95 <nathan.raynal@gmail.com>
2024-04-29 14:09:00 +0200

    ok

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,10 +2,8 @@
 """
 List all documents in a collection
 """
-import pprint
 
 
 def list_all(mongo_collection):
     """ List documents """
-    for doc in mongo_collection.find():
-        pprint.pprint(doc)
+    return mongo_collection.find()
