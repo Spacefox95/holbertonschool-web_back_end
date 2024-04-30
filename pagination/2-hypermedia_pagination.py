0b324be226aff5330e5a4914b2c17391e0ba1bdb
Spacefox95 <nathan.raynal@gmail.com>
2024-04-30 14:11:23 +0200

    get_page_hyper

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Class that paginate a database
+"""
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        if start_index < len(dataset):
+            return dataset[start_index:end_index]
+        return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        dataset = self.get_page(page, page_size)
+        total_page = len(self.dataset()) / page_size
+        next_page = None
+        if page < total_page:
+            next_page = page + 1
+        prev_page = None
+        if page > 1:
+            prev_page = page - 1
+        diction = {'page_size': page_size,
+                   'page': page,
+                   'data': dataset,
+                   'next_page': next_page,
+                   'prev_page': prev_page,
+                   'total_pages': math.ceil(total_page)}
+        return diction
