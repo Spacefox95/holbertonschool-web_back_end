abbd78697ce3fb9629dd2ea4e8bbfe43fddcfad7
Spacefox95 <nathan.raynal@gmail.com>
2024-05-06 15:09:50 +0200

    For_of

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
+}
