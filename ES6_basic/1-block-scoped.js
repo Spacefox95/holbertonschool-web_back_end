da57f0aee081cae7eff7b4ac6f6ab36730dd926b
Spacefox95 <nathan.raynal@gmail.com>
2024-05-06 11:40:13 +0200

    ok

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
