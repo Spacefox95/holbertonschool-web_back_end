244d2c50af77330afe5ba471c4cd1c6ed81ec8dc
Spacefox95 <nathan.raynal@gmail.com>
2024-05-08 10:21:34 +0200

    CloneCar

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const NewCar = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewCar;
+    return clone;
+  }
+}
