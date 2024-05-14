294ecfa8fe94daa09396e37687cef370e9566594
Spacefox95 <nathan.raynal@gmail.com>
2024-05-14 16:08:14 +0200

    Try_again

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString());
   }
   queue.push('Guardrail was processed');
   return queue;
