2a0670b5c4d03ec17a2f1689704ec6302425bc5b
Spacefox95 <nathan.raynal@gmail.com>
2024-05-14 15:48:05 +0200

    other_way

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userName = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+  const userPhoto = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+  return Promise.resolve([userName, userPhoto]);
 }
