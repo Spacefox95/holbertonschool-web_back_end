cc649710f6ee9c624f8de28ffe4b371818efd76a
Spacefox95 <nathan.raynal@gmail.com>
2024-05-14 15:39:15 +0200

    Settle

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+}
