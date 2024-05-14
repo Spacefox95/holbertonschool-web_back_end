1e01b2a45276673c93d1b011b0346b3977cc08e5
Spacefox95 <nathan.raynal@gmail.com>
2024-05-14 15:32:49 +0200

    Photo_reject

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
