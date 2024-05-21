a7eb0e86e0c7ef135b9dd5fd2613b0e9cdd54c12
Spacefox95 <nathan.raynal@gmail.com>
2024-05-21 09:59:31 +0200

    JS array

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+}
+
+export default getListStudents;
