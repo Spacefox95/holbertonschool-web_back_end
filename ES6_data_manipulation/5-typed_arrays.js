5ff43b7bcd4f56a24a86a498ed4231ddd1ee2b46
Spacefox95 <nathan.raynal@gmail.com>
2024-05-21 11:42:22 +0200

    Typed_array_dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
 function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
+  const dv1 = new DataView(buffer);
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
   view[position] = value;
-  return buffer;
+  return dv1;
 }
 export default createInt8TypedArray;
