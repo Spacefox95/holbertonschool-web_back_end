fc8f2eb41de879a956bb03bc3d9a9f198ad97428
Spacefox95 <nathan.raynal@gmail.com>
2024-05-21 14:57:24 +0200

    Grocerie_list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+function groceriesList() {
+  const list = new Map();
+  list.set('Apples', 10);
+  list.set('Tomatoes', 10);
+  list.set('Pasta', 1);
+  list.set('Rice', 1);
+  list.set('Banana', 5);
+  return list;
+}
+export default groceriesList;
