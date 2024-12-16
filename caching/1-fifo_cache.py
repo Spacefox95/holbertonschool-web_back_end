e5a5f67796f079317dd69f055a12a8227f4584eb
Spacefox95 <nathan.raynal@gmail.com>
2024-12-16 10:55:39 +0100

    ok

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -20,7 +20,7 @@ class FIFOCache(BaseCaching):
                 self.order.append(key)
             self.cache_data[key] = item
 
-        if len(self.cache_data > BaseCaching.MAX_ITEMS):
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             oldest_key = self.order.pop(0)
             del self.cache_data[oldest_key]
             print(f'DISCARD: {oldest_key}')
