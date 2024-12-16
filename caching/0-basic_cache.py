eccf254dff1c9c51cd08945cbcf81a8a8906fa90
Spacefox95 <nathan.raynal@gmail.com>
2024-12-16 10:15:26 +0100

    ok

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -16,6 +16,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Return the value linked to key in the cache data."""
-        if not key or key is None:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
