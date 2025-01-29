0a1b7e7c72f592915c3dba2ebbf6d9b67632c1d5
Spacefox95 <nathan.raynal@gmail.com>
2025-01-29 13:52:26 +0100

    ok

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -24,12 +24,16 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """ Class constructors"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Filter the values in incoming log records"""
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
-        return super().format(record)
+        redacted_message = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        record_copy = logging.LogRecord(
+            record.name, record.levelno, record.pathname, record.lineno,
+            redacted_message, record.args, record.exc_info
+        )
+        return super().format(record_copy)
