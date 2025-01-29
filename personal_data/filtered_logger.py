7ee3e88b299b64645768eeb591f63b1abb6725c7
Spacefox95 <nathan.raynal@gmail.com>
2025-01-29 10:31:47 +0100

    ok

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,15 +4,13 @@ Function to filter the message
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Filter the datum
-    """
-    escape_filter = [
-        re.escape(f) + r'=[^' + re.escape(separator) + r']*' for f in fields
-    ]
-    filter = f"({'|'.join(escape_filter)})"
-    return re.sub(filter, lambda match: match.group(0)
-                  .split('=')[0] + f"={redaction}", message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''Filter the datum '''
+    for f in fields:
+        message = re.sub(f + "=.*?" + separator,
+                         f + "=" + redaction + separator, message)
+    return message
