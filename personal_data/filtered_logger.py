19a2bc983e846a801dae5328d02a1a0df790c657
Spacefox95 <nathan.raynal@gmail.com>
2025-01-29 13:28:22 +0100

    ok

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Function to filter the message
 """
 
+import logging
 import re
 from typing import List
 
@@ -14,3 +15,19 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f + "=.*?" + separator,
                          f + "=" + redaction + separator, message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
