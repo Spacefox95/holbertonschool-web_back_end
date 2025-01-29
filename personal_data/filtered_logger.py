46fc12d6024f4fd7ee5a84e80b30e11b2d771896
Spacefox95 <nathan.raynal@gmail.com>
2025-01-29 13:47:32 +0100

    ok

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''Filter the datum '''
+    """Filter the datum"""
     for f in fields:
         message = re.sub(f + "=.*?" + separator,
                          f + "=" + redaction + separator, message)
@@ -25,9 +25,11 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """ Class constructors"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
-        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+        """ Filter the values in incoming log records"""
+        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
