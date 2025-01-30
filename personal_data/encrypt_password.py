7a2f7637b6539bec21b29007cfaf00c2ac7a194f
Spacefox95 <nathan.raynal@gmail.com>
2025-01-30 09:57:24 +0100

    ok

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     """ Hashing function """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check is the given password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
