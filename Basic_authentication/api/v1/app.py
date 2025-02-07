8d03e86d1998bc91a3d9ad09718e0cd4ab94a2cb
Spacefox95 <nathan.raynal@gmail.com>
2025-02-07 13:42:12 +0100

    ok

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,10 +2,11 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 
@@ -13,25 +14,45 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type:
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+
+
+@app.before_request
+def load_user():
+    """Load the user authorization"""
+    p_path = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    if auth is None:
+        return
+    if request.path in p_path:
+        return
+    if not auth.require_auth(request.path, p_path):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Not allowed
-    """
+    """Not allowed"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
-    """Not authorized
-    """
+    """Not authorized"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
-""" API authentification
-"""
+"""API authentification"""
+
 from typing import List, TypeVar
-from flask import request
 
 
-class Auth():
-    """ Authentification class
-    """
+class Auth:
+    """Authentification class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ path for authorization"""
+        """path for authorization"""
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        slash_path = path if path.endswith('/') else path + '/'
+        slash_path = path if path.endswith("/") else path + "/"
         if slash_path in excluded_paths:
             return False
+        print(f"Checking authentication for path: {path}")
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ flask request object"""
-        return None
+        """flask request object"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ flask request object"""
+    def current_user(self, request=None) -> TypeVar("User"):
+        """flask request object"""
         return None
