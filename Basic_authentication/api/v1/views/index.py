73efb22770839b1e6a4a652ec665f98365576b72
Spacefox95 <nathan.raynal@gmail.com>
2025-02-07 10:18:31 +0100

    ok

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -7,8 +7,7 @@ from api.v1.views import app_views
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slash=False)
 def unauthorized():
-    """ GET /api/v1/unauthorized
-    """
+    """Not authorized"""
     abort(401)
 
 
