620d24d41d665fb4e0dfe0baef7afe683cf013ac
Spacefox95 <nathan.raynal@gmail.com>
2025-02-07 10:50:02 +0100

    ok

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,14 +4,18 @@
 from typing import List, TypeVar
 from flask import request
 
+
 class Auth():
     """ Authentification class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ path for authorization"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
+        """ flask request object"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """ flask request object"""
         return None
