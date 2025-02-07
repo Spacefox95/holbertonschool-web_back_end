2bbd33c41b63e928b2ed98a2f3f14f463cc7b9e0
Spacefox95 <nathan.raynal@gmail.com>
2025-02-07 14:04:39 +0100

    ok

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class for authentification with basic encoding"""
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Get the basic authorization"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        extracted = authorization_header.split(" ", 1)
+        return extracted[1]
--- Basic_authentication/main_0.py
+++ Basic_authentication/main_0.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" Main 1
-"""
-from api.v1.auth.auth import Auth
+"""Main 2"""
 
-a = Auth()
+from api.v1.auth.basic_auth import BasicAuth
 
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
+a = BasicAuth()
+
+print(a.extract_base64_authorization_header(None))
+print(a.extract_base64_authorization_header(89))
+print(a.extract_base64_authorization_header("Holberton School"))
+print(a.extract_base64_authorization_header("Basic Holberton"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.extract_base64_authorization_header("Basic1234"))
