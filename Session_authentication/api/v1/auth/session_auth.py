2c447f614d43ee3fe65c5bf1407a6a691cf75025
Spacefox95 <nathan.raynal@gmail.com>
2025-02-21 10:20:20 +0100

    ok

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,18 @@
 Class for encoding ?
 """
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Class for session authentification"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session ID for a given user ID"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
