96dda75a56c4c4491e758b35fd2abac66c5f0a94
Spacefox95 <nathan.raynal@gmail.com>
2025-02-21 10:24:43 +0100

    ok

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Create a user ID based on the session ID"""
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
