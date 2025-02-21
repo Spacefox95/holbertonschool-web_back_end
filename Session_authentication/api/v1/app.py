0710d13d90e59dde4581c4701f552a927d3e985f
Spacefox95 <nathan.raynal@gmail.com>
2025-02-21 10:02:02 +0100

    Me_authentification

---
+++ Session_authentication/api/v1/app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type:
+    from api.v1.auth.auth import Auth
+    from api.v1.auth.basic_auth import BasicAuth
+
+    if auth_type == "basic_auth":
+        auth = BasicAuth()
+    else:
+        auth = Auth()
+
+
+@app.before_request
+def load_user():
+    """Load the user authorization"""
+    p_path = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    if auth is None:
+        return
+    if request.path in p_path:
+        return
+    if not auth.require_auth(request.path, p_path):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
+    request.current_user = auth.current_user(request)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Not allowed"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """Not authorized"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Class for encoding ?
+"""
+
+import base64
+import binascii
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """Class for authentification with basic encoding"""
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Get the basic authorization"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        extracted = authorization_header.split(" ", 1)
+        return extracted[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ Decode the basic authorization"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            b = base64.b64decode(base64_authorization_header)
+            return b.decode("utf-8")
+        except (binascii.Error, UnicodeDecodeError):
+            return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract the user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        extracted = decoded_base64_authorization_header.split(":", 1)
+        return extracted[0], extracted[1]
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+            ) -> TypeVar('User'):
+        """ Extract the user credentials"""
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        try:
+            user_creds = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not user_creds or len(user_creds) == 0:
+            return None
+        user = user_creds[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Main function to check the user credentials"""
+        header_auth = self.authorization_header(request)
+        extract_64 = self.extract_base64_authorization_header(header_auth)
+        decode_64 = self.decode_base64_authorization_header(extract_64)
+        user = self.extract_user_credentials(decode_64)
+        user_email, user_pwd = user
+        return self.user_object_from_credentials(user_email, user_pwd)
