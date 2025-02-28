06fa87fd6105327b7e4a780840a7807f07f01dba
Spacefox95 <nathan.raynal@gmail.com>
2025-02-28 09:52:53 +0100

    ok

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 """
 
 import os
-from flask import jsonify, request
+from flask import abort, jsonify, request
 
 from models.user import User
 from api.v1.views import app_views
@@ -41,3 +41,19 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route(
+        'auth_session/logout',
+        methods=['DELETE'],
+        strict_slashes=False
+        )
+def logout():
+    """Handles user logout (session destruction)"""
+
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
