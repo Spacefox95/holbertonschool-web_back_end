83481c93dd4e657538b3547a6d84ba9b4da377c2
Spacefox95 <nathan.raynal@gmail.com>
2025-03-07 14:02:33 +0100

    ok

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,16 +3,32 @@
 Route module for the API
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
-def payload_form() -> str:
+def home() -> str:
     """ GET /"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
