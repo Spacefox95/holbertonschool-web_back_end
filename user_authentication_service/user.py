336fc5c7dde022372fcff725a743c2650a582630
Spacefox95 <nathan.raynal@gmail.com>
2025-03-07 09:37:12 +0100

    ok

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -15,6 +15,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
-    hashed_password = Column(String)
+    hashed_password = Column(String, nullable=False)
     session_id = Column(String)
     reset_token = Column(String)
