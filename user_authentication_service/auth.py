99e141fc8fe6dbb3c0cceca2336ab38ceecdb9a5
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 10:53:55 +0100

    ok

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,3 +79,15 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ Detroy the user session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generates a reset password token for the given user's email"""
+        try:
+            user = self._db.find_user_by(email)
+        except NoResultFound:
+            raise ValueError('User does not exist')
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user_id=user.id, reset_token=reset_token)
+
+        return reset_token
