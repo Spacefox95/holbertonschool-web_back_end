e8a403b364707bf9c1e7682f6e1aae789d4cf948
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 13:08:00 +0100

    ok

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,9 +85,9 @@ class Auth:
         try:
             user = self._db.find_user_by(email)
         except NoResultFound:
-            raise ValueError('User does not exist')
+            raise ValueError
 
         reset_token = _generate_uuid()
-        self._db.update_user(user_id=user.id, session_id=reset_token)
+        self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
