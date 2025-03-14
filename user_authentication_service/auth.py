ef1438ad366f26f40be53c113f7f597d6be8f158
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 13:38:41 +0100

    ok

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -95,7 +95,7 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
 
-        return
+        return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
         """
