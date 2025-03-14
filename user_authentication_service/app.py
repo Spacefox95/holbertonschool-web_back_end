5754bbeb92fc80dd97729dc5a74d571b53d07ea8
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 13:12:43 +0100

    k

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -77,5 +77,17 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    email = request.form.get('email')
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
