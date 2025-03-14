92c49f5b993fb393fd1dea9b51c46050bf5738ca
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 10:03:12 +0100

    ok

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -52,6 +52,8 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
