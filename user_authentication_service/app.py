8239f534bef4a0f79432824bcfa33677f19a68b4
Spacefox95 <nathan.raynal@gmail.com>
2025-03-14 13:23:47 +0100

    ok

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -89,5 +89,22 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -117,7 +117,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-        hashed_password = self._hash_password(password)
+        hashed_password = _hash_password(password)
 
         self._db.update_user(
             user.id, hashed_password=hashed_password, reset_token=None)
