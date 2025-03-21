0766edd3ce5006de35ad162d2fdd7d665a47c7ce
Spacefox95 <nathan.raynal@gmail.com>
2025-03-21 15:46:22 +0100

    ok

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
 
 from unittest import TestCase
+
+import requests
 
 import utils
 
 
 class TestAccessNestedMap(TestCase):
-    """ Unittest class for utils"""
+    """ Unittest class for utils.access_nested_map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -27,6 +30,22 @@ class TestAccessNestedMap(TestCase):
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
 
+class TestGetJson(unittest.TestCase):
+    """ Unittest class for utils.access_nested_map"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = utils.get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
