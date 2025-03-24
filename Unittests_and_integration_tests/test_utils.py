50c2397286b36e6b863d261c7585dbed4db1e86e
Spacefox95 <nathan.raynal@gmail.com>
2025-03-24 09:46:58 +0100

    ok

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,7 @@ class TestAccessNestedMap(TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access_nested_map function"""
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
