b0ba632f0ffec72fb18e7116025611ab90511cde
Spacefox95 <nathan.raynal@gmail.com>
2025-03-24 11:02:15 +0100

    ok

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -70,6 +70,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(mocked_repos_url)
             self.assertEqual(result, expected)
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Test the has_license method"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
