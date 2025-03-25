5ea0b3e218c338560b54a64bf131f5b7bd646a41
Spacefox95 <nathan.raynal@gmail.com>
2025-03-25 10:50:23 +0100

    ok

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -27,7 +27,7 @@ babel.init_app(app, locale_selector=get_locale)
 @app.route('/')
 def index():
     """ Return to the main page"""
-    return render_template('-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
