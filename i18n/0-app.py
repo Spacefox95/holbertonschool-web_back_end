440f736b7692e96685b7111b99fcde3914920954
Spacefox95 <nathan.raynal@gmail.com>
2025-03-25 09:55:59 +0100

    ok

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	title = {'title' : 'Welcome to Holberton'}
-	h1 = {'h1' : 'Hello World'}
-	return '''<html>
-    <head>
-        <title>title['title']</title>
-    </head>
-    <body>
-        <h1>h1['h1']</h1>
-    </body>
-</html>'''
+	return render_template('index.html')
