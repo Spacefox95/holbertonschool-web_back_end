0c55a8963cb6e2653f6d3ac15e9509acce4046cf
Spacefox95 <nathan.raynal@gmail.com>
2025-03-25 10:17:04 +0100

    ok

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ Return to the main page"""
-    title = "Welcome to Holberton"
-    h1 = 'Hello world'
 
-    return render_template('0-index.html',
-                           title=title,
-                           h1=h1)
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
