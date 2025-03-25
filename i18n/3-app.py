86cdb9855bd9cb38abe33530df77ba011bb24707
Spacefox95 <nathan.raynal@gmail.com>
2025-03-25 11:21:28 +0100

    ok

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -3,11 +3,7 @@
 
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
-
-""" gettext function"""
-_ = gettext
+from flask_babel import Babel
 
 
 class Config():
