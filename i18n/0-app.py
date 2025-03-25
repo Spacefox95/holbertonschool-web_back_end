e092d4d0a138fc237d1312482a3ca72b9e0c4419
Spacefox95 <nathan.raynal@gmail.com>
2025-03-25 09:57:55 +0100

    ok

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+""" Flask server setup"""
 
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	""" Return to the main page"""
+	return render_template('./templates/index.html')
