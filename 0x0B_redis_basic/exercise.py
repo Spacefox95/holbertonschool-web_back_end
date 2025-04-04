06ad75f6cd9e8f22820f012ef6129260d8900005
Spacefox95 <nathan.raynal@gmail.com>
2025-04-04 11:08:08 +0200

    ok

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 
 """ Cache class for redis"""
 
-from typing import Union
+from typing import Callable, Optional, Union
 import uuid
 import redis
 
@@ -20,3 +20,26 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[
+                str,
+                bytes,
+                int,
+                float, None]:
+        """ Get the value"""
+        if key:
+            value = self._redis.get(key)
+            if fn:
+                return fn(value)
+            return value
+        return None
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ Get the value for a str key"""
+        return self.get(key=key, fn=lambda b: b.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ Get the value for an int key"""
+        return self.get(key=key, fn=int)
