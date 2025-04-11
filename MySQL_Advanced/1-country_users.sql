4e0e2ef7fc0171338a996762d5f3ea2c703dd416
Spacefox95 <nathan.raynal@gmail.com>
2025-04-11 10:06:56 +0200

    ok

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create the 'users' table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+		id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		email VARCHAR(255) UNIQUE NOT NULL,
+		name VARCHAR(255),
+		country ENUM ('US', 'CO', 'TN') NOT NULL
+	);
