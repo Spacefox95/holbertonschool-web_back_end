8631e7bd485383b826e720ece203be412bd3a0ab
Spacefox95 <nathan.raynal@gmail.com>
2025-04-18 09:42:41 +0200

    ok

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,27 +1,33 @@
-DELIMITER //
+-- Create a procedure to add a new correction for student
+DELIMITER / / CREATE PROCEDURE AddBonus (
+	IN p_user_id INT,
+	IN p_project_name VARCHAR(255),
+	IN p_score INT
+) BEGIN DECLARE v_project_id INT;
 
-CREATE PROCEDURE AddBonus (
-    IN p_user_id INT,
-    IN p_project_name VARCHAR(255),
-    IN p_score INT
-)
-BEGIN
-    DECLARE v_project_id INT;
+SELECT
+	id INTO v_project_id
+FROM
+	projects
+WHERE
+	name = p_project_name
+LIMIT
+	1;
 
-    SELECT id INTO v_project_id
-    FROM projects
-    WHERE name = p_project_name
-    LIMIT 1;
+IF v_project_id IS NULL THEN
+INSERT INTO
+	projects (name)
+VALUES
+	(p_project_name);
 
-    IF v_project_id IS NULL THEN
-        INSERT INTO projects(name)
-        VALUES (p_project_name);
+SET
+	v_project_id = LAST_INSERT_ID ();
 
-        SET v_project_id = LAST_INSERT_ID();
-    END IF;
+END IF;
 
-    INSERT INTO corrections(user_id, project_id, score)
-    VALUES (p_user_id, v_project_id, p_score);
-END //
+INSERT INTO
+	corrections (user_id, project_id, score)
+VALUES
+	(p_user_id, v_project_id, p_score);
 
-DELIMITER ;
+END / / DELIMITER;
