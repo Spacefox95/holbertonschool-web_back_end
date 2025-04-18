d302bed1a486e7a8463eeac976d5f38e68b8fcfd
Spacefox95 <nathan.raynal@gmail.com>
2025-04-18 11:00:13 +0200

    ok

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,2 +1,10 @@
 --  function that divides the first by the second number or returns 0.
-DROP FUNCTION IF EXISTS SafeDiv CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN RETURN IF (b = 0, 0, a / b);
+DELIMITER $$
+DROP FUNCTION IF EXISTS SafeDiv$$ 
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT 
+DETERMINISTIC 
+BEGIN 
+RETURN IF (b = 0, 0, a / b);
+END$$
+DELIMITER ;
