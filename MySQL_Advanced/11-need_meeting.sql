677565860d21cd16d6f064bcad61f233aa5d5a21
Spacefox95 <nathan.raynal@gmail.com>
2025-04-18 11:09:14 +0200

    ok

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,5 @@
 --  lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name, score, last_meeting
 FROM students
