1b3594abc06aa2be5ab601296328ffd1555069ff
Spacefox95 <nathan.raynal@gmail.com>
2025-04-25 11:11:42 +0200

    ok

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,14 +6,23 @@ describe("calculateNumber", function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it("Sum when a is a float.", () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
   it("Sum when b is a float.", () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-	it("Sum when both are float and a is with .5", () => {
-		assert.strictEqual(calculateNumber(1.5, 3.7), 6)
-	})
+
+  it("Sum when a is rounded down & b is rounded up.", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it("Sum when a is a float.", () => {
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
+  });
+
+  it("Sum when a is rounded up & b is rounded down.", () => {
+    assert.strictEqual(calculateNumber(1.7, 3.2), 5);
+  });
+
+  it("Sum when both are rounded up.", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
