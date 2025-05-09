ca77d67fb5caf7606047ee252291472d04f84d63
Spacefox95 <nathan.raynal@gmail.com>
2025-05-09 15:44:55 +0200

    ok

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -5,13 +5,11 @@ describe("getPaymentTokenFromAPI", function () {
   it("should return a resolved promise with correct data when success is true", function (done) {
     getPaymentTokenFromAPI(true)
       .then((response) => {
-        expect(response).to.be.an("object");
-        expect(response).to.have.property(
-          "data",
-          "Successful response from the API"
-        );
+        expect(response).to.deep.equal({
+          data: "Successful response from the API",
+        });
         done();
       })
-      .catch(done);
+      .catch((error) => done(error));
   });
 });
