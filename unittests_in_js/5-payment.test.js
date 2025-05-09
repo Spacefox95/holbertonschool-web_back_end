641fba9fe3546765333684f241c75bbd19b4c817
Spacefox95 <nathan.raynal@gmail.com>
2025-05-09 15:26:51 +0200

    ok

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -5,33 +5,28 @@ const Utils = require("./utils.js");
 const sendPaymentRequestToApi = require("./3-payment.js");
 
 describe("sendPaymentRequestToApi", function () {
-  let stub;
   let spy;
 
   this.beforeEach(() => {
-    stub = sinon.stub(Utils, "calculateNumber").returns(10);
     spy = sinon.spy(console, "log");
   });
 
   this.afterEach(() => {
-    stub.restore();
     spy.restore();
   });
 
   it("should call Utils.calculateNumber with correct args", () => {
     sendPaymentRequestToApi(100, 20);
 
-    expect(stub.calledOnce).to.be.true;
-    expect(stub.calledWith("SUM", 100, 20)).to.be.true;
+    expect(spy.calledOnce).to.be.true;
     expect(spy.calledOnceWithExactly("The total is: 120")).to.be.true;
 
     spy.restore();
   }),
     it("should call Utils.calculateNumber with correct args", () => {
-      sendPaymentRequestToApi(100, 20);
+      sendPaymentRequestToApi(10, 10);
 
-      expect(stub.calledOnce).to.be.true;
-      expect(stub.calledWith("SUM", 10, 10)).to.be.true;
+      expect(spy.calledOnce).to.be.true;
       expect(spy.calledOnceWithExactly("The total is: 20")).to.be.true;
     });
 });
