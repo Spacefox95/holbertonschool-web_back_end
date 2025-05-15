c7a6f631c620af90cadc8aee92747dd9a6c3a9d2
Spacefox95 <nathan.raynal@gmail.com>
2025-05-15 17:44:52 +0200

    ok

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,11 @@
-const { assert, expect } = require("chai");
+const chai = require("chai");
 const getPaymentTokenFromAPI = require("./6-payment_token.js");
 
 describe("getPaymentTokenFromAPI", function () {
   it("should return a resolved promise with correct data when success is true", function (done) {
     getPaymentTokenFromAPI(true)
       .then((response) => {
-        expect(response).to.deep.equal({
+        chai.expect(response).to.deep.equal({
           data: "Successful response from the API",
         });
         done();
