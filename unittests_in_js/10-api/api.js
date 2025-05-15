cd0f631bcc77c8d86a5d56db0f2c6a5c7fd189a4
Spacefox95 <nathan.raynal@gmail.com>
2025-05-15 17:54:38 +0200

    ok

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -3,8 +3,25 @@ const port = 7865;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", ( res) => {
 	res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+	res.status(200).send("Payment methods for cart " + req.params.id);
+});
+
+app.get("/available_payments", (req, res) => {
+	res.status(200).send({
+		payment_methods: {
+			credit_cards: true,
+			paypal: false
+		}
+	});
+});
+
+app.post("/login", (req, res) => {
+	res.status(200).send("Welcome " + req.body.userName);
 });
 
 app.listen(port, () => {
--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -12,4 +12,47 @@ describe("indexPageTest", function () {
 			done();
 		});
 	});
-});
+
+	it("Should return the correct body", function (done) {
+		request.get("http://localhost:7865/cart/10", (error, response, body) => {
+			chai.expect(response.statusCode).to.equal(200);
+			chai.expect(body).to.equal("Payment methods for cart " + 10);
+
+			if (error) return done(error);
+
+			done();
+		});
+	});
+
+	it("Should work as expected for endpoint GET /available_payements", function (done) {
+		request.get("http://localhost:7865/available_payments", (error, response, body) => {
+			chai.expect(response.statusCode).to.equal(200);
+			chai.expect(body).to.deep.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+
+			if (error) return done(error);
+
+			done();
+		});
+	})
+
+	it("should work as expected with POST /login endpoint", function (done) {
+		request.post("http://localhost:7865/login", { json: { "userName": "Betty" } }, (error, response, body) => {
+			chai.expect(response.statusCode).to.equal(200);
+			chai.expect(body).to.equal("Welcome Betty");
+
+			if (error) return done(error);
+
+			done();
+		});
+	})
+
+	it("Should return 404 when id is not numeric", function (done) {
+		request.get("http://localhost:7865/cart/hello", (error, response, body) => {
+			chai.expect(response.statusCode).to.equal(404);
+
+			if (error) return done(error);
+
+			done();
+		});
+	});
+})
