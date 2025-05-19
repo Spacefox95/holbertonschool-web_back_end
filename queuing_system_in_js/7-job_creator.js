66723fbed068b365ff73ff6c765c44db9d4c341b
Spacefox95 <nathan.raynal@gmail.com>
2025-05-19 15:21:00 +0200

    ok

---
+++ queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,75 @@
+import { createQueue } from "kue";
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+
+const queue = createQueue()
+
+for (const data of jobs) {
+	
+const job = queue
+  .create("push_notification_code_2", data)
+  .save((err) => {
+    if (!err) {
+      console.log("Notification job created:", job.id);
+    } else {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    }
+  })
+	.on('complete', () => {
+		console.log(`Notification job ${job.id} completed`)
+	})
+
+	.on('progress', (percentage) => {
+		console.log(`Notification job ${job.id} ${percentage}% complete`)
+	})
+
+	.on('failed', (err) => {
+		console.log(`Notification job ${job.id} failed: ${err}`)
+	})
+
+}
