65761d22b9ecd255d94a5ab1ec9f8f484009fff7
Spacefox95 <nathan.raynal@gmail.com>
2025-05-19 10:25:04 +0200

    ok

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+client.on("error", (err) => console.log("Redis Client Redis client not connected to the server:", err))
+client.set("message", "Redis client connected to the server");
+client.get('message', (err, value) => {
+console.log(value)
+})
